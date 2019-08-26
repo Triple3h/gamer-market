903c95320057e1807e1b72fd4514711169a36415
Triple3h <819580048@qq.com>
2019-08-26 14:36:23 +0800

    测试上传

---
+++ src/main/java/com/qf/gamer/utils/Result.java
@@ -0,0 +1,33 @@
+package com.qf.gamer.utils;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Classname Result
+ * @Date 2019/8/22
+ * @Author Triple3H
+ */
+
+@Data
+public class Result<T> implements Serializable {
+    private int status;
+    private String msg;
+    private T data;
+
+    public static <T> Result success(T t) {
+        Result<T> result = new Result<>();
+        result.setData(t);
+        result.setMsg("success");
+        result.setStatus(200);
+        return result;
+    }
+
+    public static Result error(String err) {
+        Result result = new Result();
+        result.setMsg(err);
+        result.setStatus(404);
+        return result;
+    }
+}
