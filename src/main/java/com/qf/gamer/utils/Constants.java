c4bf5eef7d56d9fa0e57d2679f6652712c151d18
kekesimida <1006636887@qq.com>
2019-08-26 14:38:28 +0800

    测试

---
+++ src/main/java/com/qf/gamer/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.qf.gamer.utils;
+
+/**
+ * @author SongZun
+ * @date 2019/8/20 0020 下午 4:37
+ */
+public class Constants {
+    public static final String SUCCESS_MSG = "success";
+    public static final String ERROR_MSG = "error";
+    public static final int SUCCESS_STATUS_200 = 200;
+    public static final int ERROR_STATUS_404 = 404;
+}
---
+++ src/main/java/com/qf/gamer/utils/Result.java
@@ -0,0 +1,36 @@
+package com.qf.gamer.utils;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+
+/**
+ * @author SongZun
+ * @date 2019/8/17 0017 上午 11:46
+ */
+@Data
+@Component
+@RequestScope
+public class Result<T> {
+    private T data;
+    private Integer status;
+    private String msg ;
+
+    public static Result success(Object obj) {
+        Result result = new Result();
+        result.setStatus(Constants.SUCCESS_STATUS_200);
+        result.setData(obj);
+        result.setMsg(Constants.SUCCESS_MSG);
+        return result;
+    }
+
+
+    public static Result error() {
+        Result result = new Result();
+        result.setStatus(Constants.ERROR_STATUS_404);
+        result.setMsg(Constants.ERROR_MSG);
+        return result;
+    }
+
+}
