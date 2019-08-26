7334965f4963bc4c60153cfeb3a02c0bfdae6f02
SanShiGe129 <1905137498@qq.com>
2019-08-26 18:44:00 +0800

    登录功能

---
+++ src/main/java/com/qf/gamer/util/Result.java
@@ -0,0 +1,35 @@
+package com.qf.gamer.util;
+
+import lombok.Data;
+
+/**
+ * @author zhangwei
+ */
+@Data
+public class Result<T> {
+    private int status;
+    private String msg;
+    private T data;
+
+    public static Result success(Object obj) {
+        Result result = new Result();
+        result.setData(obj);
+        result.setMsg("success");
+        result.setStatus(200);
+        return result;
+    }
+
+    public static Result error() {
+        Result result = new Result();
+        return result;
+    }
+
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
