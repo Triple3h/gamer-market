25df2b28d28ced3a24e5398101a6c210c1cb9674
SanShiGe129 <1905137498@qq.com>
2019-08-26 14:38:49 +0800

    测试提交

---
+++ src/main/java/com/qf/gamer/Test.java
@@ -0,0 +1,13 @@
+package com.qf.gamer;
+
+/**
+ * @ClassName Test
+ * @Author lulei
+ * @Data 2019/8/26 14:37
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("鲁磊");
+    }
+}
